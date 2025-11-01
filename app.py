4368a0b2d247316db42ce768100d334504eef2a1
vigneshth <vigneshrpt2005@gmail.com>
2025-11-01 13:04:33 +0530

    Initial SpendSmart commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, abort, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
